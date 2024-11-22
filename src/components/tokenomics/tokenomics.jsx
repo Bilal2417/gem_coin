718ef91d03bc5b27888ff1baf88ab838d2b08d5b
Bilal Arsal <bilalarsal24@gmail.com>
2024-11-22 20:35:12 +0500

    ...

--- src/components/tokenomics/tokenomics.jsx
+++ src/components/tokenomics/tokenomics.jsx
@@ -1,7 +1,12 @@
 import "./tokenomics.css"
 import { Box , Container, Grid ,Grid2, Typography } from "@mui/material"
 import tknchart from "../../images/tknImg.png"
+import tokenchart from "../../images/toknChartImg.png"
 import tokenImg from "../../images/usingPadMain.png"
+import bLimit from "../../images/bLimit.png"
+import pShare from "../../images/pShare.png"
+import rShare from "../../images/rShare.png"
+import oPublic from "../../images/tknComp.png"
 
 export default function Tokenomics () {
     return <>
@@ -70,11 +75,12 @@ export default function Tokenomics () {
                     </Box>
 
                         <Grid container spacing={2} justifyContent={"center"} sx={{
-                            '@media ( max-width : 900px )' : {
-                                gap : "50px"}}}>
+                            maxWidth : "80% !important",
+                            margin : "auto"
+                            }}>
                             
 
-<Grid item xs={12} md={6} sx={{ justifyContent : "center" , position : "relative", zIndex : "1", position : "relative",
+<Grid item xs={12} md={6} sx={{ justifyContent : "center" , 
             //    "&:before":{
             //                            content: '""',  
             //                            backgroundImage : `url(${tknchart})`,
@@ -86,8 +92,173 @@ export default function Tokenomics () {
             //        left : "15%"
             //    },
 }} className="tokenChartBlock">
+ 
+{/* <Box sx={{position : "relative",zIndex : "1"}}> */}
+<Box sx={{position : "relative", height : "400px",
+    "&:before":{
 
-    <img src={tknchart}/>
+        width : "100%",
+        height : "100%",
+        content: '""',  
+        position: 'absolute',
+        top  : '0',
+        left : "0",
+        backgroundImage : `url(${tokenchart})`,
+        backgroundSize : "contain", 
+        backgroundRepeat : "no-repeat",
+        backgroundPosition : "center"       
+    },
+    "@media (max-width : 500px )":{
+        height : "300px"
+    },
+    "@media (max-width : 500px )":{
+        height : "200px"
+    },
+}}>
+
+    {/* <img src={tokenchart}/> */}
+</Box>
+
+    {/* <Box sx={{display : "flex", flexDirection : "column" ,gap : "35px", margin : "50px 0" , position : "absolute",top : "0", width : "130%" , zIndex : "-1"}}>
+
+    <Box sx={{display : "flex" , alignItems : "center" , marginLeft : "-100px" }}>
+        <Box>
+        <Typography variant="body1" sx={{
+            textTransform : "uppercase",
+            fontFamily : "Metal Mania",
+            color : "#7DCCF4",
+            fontSize : "24px"            ,
+            "@media (max-width : 1000px )":{
+                fontSize : "18px"            ,
+            }
+        }}>Open public sale</Typography>
+        <Typography variant="body1" sx={{
+                        textTransform : "uppercase",
+                        fontFamily : "Montserrat",
+                        color : "#fff",
+                        fontSize : "18px",
+                        "@media (max-width : 1000px )":{
+                            fontSize : "14px"            ,
+                        }
+        }}>25.80%</Typography>
+        </Box> 
+                        <Box sx={{width : "100px"}}>
+                    ,    <img src={oPublic}/>
+                        </Box>
+    </Box>
+
+<Box sx={{alignSelf : "flex-end"}}>
+
+    <Box sx={{display : "flex" , alignItems : "center"}}>
+        <Box sx={{width : "100px"}}>
+        <img src={bLimit}/>
+        </Box>
+        <Box>
+        <Typography variant="body1" sx={{
+            textTransform : "uppercase",
+            fontFamily : "Metal Mania",
+            color : "#7DCCF4",
+            fontSize : "24px"       ,
+            "@media (max-width : 1000px )":{
+                fontSize : "18px"            ,
+            }     
+        }}>burning limit</Typography>
+        <Typography variant="body1" sx={{
+                        textTransform : "uppercase",
+                        fontFamily : "Montserrat",
+                        color : "#fff",
+                        fontSize : "18px",
+                        "@media (max-width : 1000px )":{
+                            fontSize : "14px"            ,
+                        }
+                    }}>50%</Typography>
+        </Box> 
+    </Box>
+                    </Box>
+
+                    <Box sx={{display : "flex" , alignItems : "center"  ,marginLeft : "-85px"}}>
+        <Box sx={{
+                      "@media (max-width : 1000px )":{
+                        marginLeft : "-15px"           ,
+                    }
+        }}>
+        <Typography variant="body1" sx={{
+            textTransform : "uppercase",
+            fontFamily : "Metal Mania",
+            color : "#7DCCF4",
+            fontSize : "24px"      ,
+            "@media (max-width : 1000px )":{
+                fontSize : "18px"            ,
+            }      
+        }}>Presale Launch</Typography>
+        <Typography variant="body1" sx={{
+                        textTransform : "uppercase",
+                        fontFamily : "Montserrat",
+                        color : "#fff",
+                        fontSize : "18px",
+                        "@media (max-width : 1000px )":{
+                            fontSize : "14px"            ,
+                        }
+        }}>11.3%</Typography>
+        </Box> 
+                        <Box sx={{width : "100px"}}>
+                        <img src={oPublic}/>
+                        </Box>
+    </Box>
+
+    </Box>
+<Box sx={{display : "flex" , alignItems : "center" , width : "70%" , position : "absolute", bottom : "0" , zIndex : "-1" , marginBottom : "-35px"}}>
+<Box sx={{display : "flex" , alignItems : "center"}}>
+        <Box>
+        <Typography variant="body1" sx={{
+            textTransform : "uppercase",
+            fontFamily : "Metal Mania",
+            color : "#7DCCF4",
+            fontSize : "18px"        ,
+            "@media (max-width : 1000px )":{
+                fontSize : "14px"            ,
+            }    
+        }}>company share</Typography>
+        <Typography variant="body1" sx={{
+                        textTransform : "uppercase",
+                        fontFamily : "Montserrat",
+                        color : "#fff",
+                        fontSize : "18px",
+                        "@media (max-width : 1000px )":{
+                            fontSize : "12px"            ,
+                        }
+        }}>6.5%</Typography>
+        </Box> 
+    </Box>
+
+    <Box sx={{display : "flex" , alignItems : "center"}}>
+        <Box>
+            <Box sx={{width : "80px" , marginLeft : "35px" , marginTop :  "-10px"}}>
+            <img src={rShare}/>
+            </Box>
+        <Typography variant="body1" sx={{
+            textTransform : "uppercase",
+            fontFamily : "Metal Mania",
+            color : "#7DCCF4",
+            fontSize : "18px"       ,
+            "@media (max-width : 1000px )":{
+                fontSize : "14px"            ,
+            }     
+        }}>reward share</Typography>
+        <Typography variant="body1" sx={{
+                        textTransform : "uppercase",
+                        fontFamily : "Montserrat",
+                        color : "#fff",
+                        fontSize : "18px",
+                        "@media (max-width : 1000px )":{
+                            fontSize : "12px"            ,
+                        }
+        }}>11.3%</Typography>
+        </Box> 
+    </Box>
+</Box> 
+</Box> */}
+
 </Grid>
 <Grid item xs={12} md={6} className="tokenImgBlock">
     <img src={tokenImg}/>
